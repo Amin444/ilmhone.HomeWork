4b0f5371600ffa2e76a4128681185ce3a8b1eb8c
Amin Mirzoev <aminmirzoev123@gmail.com>
2020-11-13 18:03:22 +0500

    I study a new method

---
+++ Lesson 14/Hw14-5th-task/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*5) Замените в строке все вхождения 'word' на 'letter'.*/
+
+            String ChangeString="isWord ,word,word just word and word ";
+           String BigChangeString= ChangeString.toUpperCase();
+           System.out.println(BigChangeString.replace("WORD","letter"));
+        }
+
+            }
+
+    }
+
